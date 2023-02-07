ed976d78d50f72bb35dc05520257e0f96c14bf83
mrezaei <moeinrezaei330@gmail.com>
2023-02-07 14:01:54 +0100

    run.py added.

---
+++ run.py
@@ -0,0 +1,21 @@
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        fnum = int(num)
+    except:
+        print('Invalid input')
+        continue
+    if (largest == None) or (smallest == None):
+        largest = fnum
+        smallest = fnum
+    if largest < fnum:
+        largest = fnum
+    if smallest > fnum:
+        smallest = fnum
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
