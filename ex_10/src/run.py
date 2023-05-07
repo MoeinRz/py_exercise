7b773ea2d0ef030026427e4477b1cdfcd6691731
mrezaei <moeinrezaei330@gmail.com>
2023-05-07 18:56:28 +0200

    ex_10 added.

---
+++ ex_10/src/run.py
@@ -0,0 +1,26 @@
+# *************************************************************************** */
+#                                                                             */
+#                                                                             */
+#    run.py                                                                   */
+#                                                                             */
+#    By: moeinrz <moeinrezaei330@gmail.com>                                   */
+#                                                                             */
+#    Created: 2023/04/27 20:04:23 by moeinrz                                  */
+#    Updated: 2023/04/27 21:25:20 by moeinrz                                  */
+#                                                                             */
+# *************************************************************************** */
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
---
+++ ex_10/subject.md
@@ -0,0 +1,19 @@
+# py_exercise
+Exploring the HyperText Transport Protocol
+
+You are to retrieve the following document using the HTTP protocol in a way that you can examine the HTTP Response headers.
+
+http://data.pr4e.org/intro-short.txt
+There are three ways that you might retrieve this web page and look at the response headers:
+
+Preferred: write a py code to get the below data of above url in TCP protocol.
+
+Last-Modified:
+
+ETag:
+
+Content-Length:
+
+Cache-Control:
+
+Content-Type:
