45587d88041f7b8cbb1f952a6f897e8b745644dc
MoeinRz <moeinrezaei330@gmail.com>
2023-05-11 00:33:59 +0200

    ex_11 added

---
+++ ex_11/src/run2.py
@@ -0,0 +1,33 @@
+# *************************************************************************** */
+#                                                                             */
+#                                                                             */
+#    run.py                                                                   */
+#                                                                             */
+#    By: moeinrz <moeinrezaei330@gmail.com>                                   */
+#                                                                             */
+#    Created: 2023/05/07 20:04:23 by moeinrz                                  */
+#    Updated: 2023/05/07 21:25:20 by moeinrz                                  */
+#                                                                             */
+# *************************************************************************** */
+
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
